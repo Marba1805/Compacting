ef3bc98d295e70bec9085f0cbe570123836682ea
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 20:36:11 -0300

    Add files via upload

---
+++ compactador/Exe1.c
@@ -0,0 +1,19 @@
+
+
+struct comLet {
+   char let;
+   int qtd;
+   /* declarando os dados dessa estrutura. */
+};
+
+
+
+
+void compactador(v){
+	bool i = false;
+	while(i != true){
+		for(int c = 0; c<= len(v); c++){
+			
+		}
+	}
+}
---
+++ compactador/Exe3.c
@@ -0,0 +1,19 @@
+
+
+struct comLet {
+   char let;
+   int qtd;
+};
+
+
+
+
+void compactador(v){
+	bool i = false;
+	while(i != true){
+		for(int c = 0; c<= len(v); c = c + 1){
+			comLet.let = v[c];
+			
+		}
+	}
+}
