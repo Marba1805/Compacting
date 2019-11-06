b758db9692db293d59f89f5fdceafcbba85ba619
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 20:45:30 -0300

    Update Exe3.c

--- compactador/Exe3.c
+++ compactador/Exe3.c
@@ -12,8 +12,11 @@ void compactador(v){
 	bool i = false;
 	while(i != true){
 		for(int c = 0; c<= len(v); c = c + 1){
-			comLet.let = v[c];
-			
+			for(int j = 0; j<= len(v); j = j + 1)
+			var.let = v[c];
+			if (v[j] == var.let){
+				var.qtd = var.qtd + 1
+			}
 		}
 	}
 }
