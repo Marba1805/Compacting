1a4c6984f8339eaaf4749095b07cb98b62c95bce
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 20:53:48 -0300

    Update Exe3.c

--- compactador/Exe3.c
+++ compactador/Exe3.c
@@ -15,7 +15,7 @@ void compactador(v){
 	
 	bool i = false; // cria uma condicao para um "loop while" rodar mais para fente
 	struct comLet var; //cria uma variavel com a estrutura criada anteriormente
-	struct cpmLet lt[contPadr(v)] /*cria um vetor de tamanho exatamente nescecario ao caso*/
+	struct comLet lt[contPadr(v)] /*cria um vetor de tamanho exatamente nescecario ao caso*/
 	while(i != true){
 		for(int c = 0; c<= len(v); c = c + 1){
 			for(int j = 0; j<= len(v); j = j + 1)
