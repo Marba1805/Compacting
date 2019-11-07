f35f9a25c7865ba52f2bdd4cf2a809485480a2eb
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 21:30:18 -0300

    Update Exe1.c

--- compactador/Exe1.c
+++ compactador/Exe1.c
@@ -1,19 +1,42 @@
-
+#include <stdio.h>
 
 struct comLet {
    char let;
    int qtd;
-   /* declarando os dados dessa estrutura. */
 };
 
+int contPadr(v){
+	//Essa funcao serve para contar quantos padroes tem no vetor de input
+	int cont = 0;
+		for(int c = 0; c<= len(v); c = c + 1){
+			for(int j = 0; j<= len(v); j = j + 1)
+			char a = v[c];
+			if (v[j] == a){
+			}
+			else{	
+				cont = cont +1;
+			}
+		}
+}
 
 
 
 void compactador(v){
-	bool i = false;
+	
+	bool i = false; // cria uma condicao para um "loop while" rodar mais para fente
+	struct comLet var; //cria uma variavel com a estrutura criada anteriormente
+	struct comLet lt[contPadr(v)] /*cria um vetor de tamanho exatamente nescecario ao caso*/
 	while(i != true){
-		for(int c = 0; c<= len(v); c++){
-			
+		for(int c = 0; c<= len(v); c = c + 1){
+			for(int j = 0; j<= len(v); j = j + 1)
+			var.let = v[c];
+			if (v[j] == var.let){
+				var.qtd = var.qtd + 1;
+			}
+			else{
+				
+				break;
+			}
 		}
 	}
 }
