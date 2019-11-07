6aa930384f7b827768e646805a1e0f2020bcf94c
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 21:25:05 -0300

    Update Exe3.c

--- compactador/Exe3.c
+++ compactador/Exe3.c
@@ -6,7 +6,17 @@ struct comLet {
 };
 
 int contPadr(v){
-	
+	//Essa funcao serve para contar quantos padroes tem no vetor de input
+	int cont = 0;
+		for(int c = 0; c<= len(v); c = c + 1){
+			for(int j = 0; j<= len(v); j = j + 1)
+			char a = v[c];
+			if (v[j] == a){
+			}
+			else{	
+				cont = cont +1;
+			}
+		}
 }
 
 
