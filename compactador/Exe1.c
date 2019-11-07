310a41449e27f38f1a37c32de644a926898c6b1f
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-06 22:27:43 -0300

    Update Exe1.c

--- compactador/Exe1.c
+++ compactador/Exe1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 struct comLet {
-   char let;
-   int qtd;
-};
-
-int len(v){
-//Retorna o tamanho do vetor
-	int len = sizeof(v) / sizeof(v[0]);
-	return len; 
+   char let;//caracter ou  combinacao de caracteres
+   int qtd;// quantidade de vezes que ele repete
 }
 
+int len(v){
+	
+int len = sizeof(v) / sizeof(v[0]);
+return len; 
+}
+
+
 int contPadr(v){
-//Conta quantos padroes tem no vetor de input
+	//Essa funcao serve para contar quantos padroes tem no vetor de input
 	int cont = 0;
-	int c,j;
-		for(c = 0; c<= len(v); c = c + 1){
-			for(j = 0; j<= len(v); j = j + 1)
+		for(int c = 0; c<= len(v); c = c + 1){
+			for(int j = 0; j<= len(v); j = j + 1)
 			char a = v[c];
 			if (v[j] == a){
 			}
@@ -33,11 +33,9 @@ void compactador(v){
 	bool i = false; // cria uma condicao para um "loop while" rodar mais para fente
 	struct comLet var; //cria uma variavel com a estrutura criada anteriormente
 	struct comLet lt[contPadr(v)] /*cria um vetor de tamanho exatamente nescecario ao caso*/
-	int c,j;
-	
 	while(i != true){
-		for(c = 0; c<= len(v); c = c + 1){
-			for(j = 0; j<= len(v); j = j + 1)
+		for(int c = 0; c<= len(v); c = c + 1){
+			for(int j = 0; j<= len(v); j = j + 1)
 			var.let = v[c];
 			if (v[j] == var.let){
 				var.qtd = var.qtd + 1;
@@ -48,21 +46,4 @@ void compactador(v){
 			}
 		}
 	}
-}
-
-void main(){
-	
-	char teste[10];
-	teste[0] = a;
-	teste[1] = a;
-	teste[2] = a;
-	teste[3] = b;
-	teste[4] = b;
-	teste[5] = c;
-	teste[6] = c;
-	teste[7] = c;
-	teste[8] = c;
-	teste[9] = c;
-	
-	printf(contPadr(teste))
 }
