b873c9fd1d4391b2453463ef88206aa22f898f54
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-08 10:09:10 -0300

    Update Exe3.c

--- compactador/Exe3.c
+++ compactador/Exe3.c
@@ -25,18 +25,20 @@ int contPadr(v){
 				cont = cont +1;
 			}
 		}
+	return cont;
 }
 
 
 
 void compactador(v){
 	
-	bool i = false; // cria uma condicao para um "loop while" rodar mais para fente
+	int i = 0; // cria uma condicao para um "loop while" rodar mais para fente
+	int padr = contPadr(v); //cria uma variavel com o numero de padroes no vetor
 	struct comLet var; //cria uma variavel com a estrutura criada anteriormente
-	struct comLet lt[contPadr(v)] /*cria um vetor de tamanho exatamente nescecario ao caso*/
+	struct comLet lt[padr] /*cria um vetor de tamanho exatamente nescecario ao caso*/
 	int c,j;
 	
-	while(i != true){
+	while(i <= padr){
 		for(c = 0; c<= len(v); c = c + 1){
 			for(j = 0; j<= len(v); j = j + 1)
 			var.let = v[c];
@@ -53,17 +55,4 @@ void compactador(v){
 
 void main(){
 	
-	char teste[10];
-	teste[0] = a;
-	teste[1] = a;
-	teste[2] = a;
-	teste[3] = b;
-	teste[4] = b;
-	teste[5] = c;
-	teste[6] = c;
-	teste[7] = c;
-	teste[8] = c;
-	teste[9] = c;
-	
-	printf(contPadr(teste))
 }
