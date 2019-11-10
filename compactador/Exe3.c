dfd3893877dd6443878a63e25ddc8ed4f20a4d35
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-11-10 02:01:44 -0300

    Update Exe3.c

--- compactador/Exe3.c
+++ compactador/Exe3.c
@@ -7,19 +7,26 @@ struct comLet {
 
 
 int len(v){
-//Retorna o tamanho do vetor
+/*Retorna o tamanho do vetor
+essencailmente a funcao len presente em outras linguagens*/
 	int len = sizeof(v) / sizeof(v[0]);
 	return len; 
 }
 
 int contPadr(v){
 //Conta quantos padroes tem no vetor de input
+	char aux[(len(v)/2)];
 	int cont = 0;
-	int c,j;
+	int c,j,k;
 		for(c = 0; c<= len(v); c = c + 1){
-			for(j = 0; j<= len(v); j = j + 1)
-			char a = v[c];
-			if (v[j] == a){
+			for(j = c+1; j<= len(v); j = j + 1)
+			aux[a] = v[c];
+			if (v[j] == aux[a]){
+				for(k =0; k  <= c+1; k=k+1){
+					if (){ // esse loop tem como objetivo verificar 
+					       //o resto do vetor auxiliar para conferir o padrao
+					}	
+				}
 			}
 			else{	
 				cont = cont +1;
@@ -36,20 +43,31 @@ void compactador(v){
 	int padr = contPadr(v); //cria uma variavel com o numero de padroes no vetor
 	struct comLet var; //cria uma variavel com a estrutura criada anteriormente
 	struct comLet lt[padr] /*cria um vetor de tamanho exatamente nescecario ao caso*/
-	int c,j;
+	char aux[(len(v)/2)]; //cira um vetor de auxilio de tamanho maximo de metade do vetor inicial
+	int c,j,k,count;
+	
+	
 	
 	while(i <= padr){
 		for(c = 0; c<= len(v); c = c + 1){
-			for(j = 0; j<= len(v); j = j + 1)
-			var.let = v[c];
-			if (v[j] == var.let){
-				var.qtd = var.qtd + 1;
-			}
-			else{
+			for(j = c+1; j<= len(v); j = j + 1)
+				if (v[c] !== v[j]){
+					aux[c] = c[c];
+				}
+
+				else{
 				
 				break;
+				}
+			for(k =0; k  <= c+1; k=k+1){   // esse loop tem como objetivo verificar
+				if (v[c] == aux[k]){   //o resto do vetor auxiliar para conferir o padrao
+					count = count +1;
+				}
 			}
-		}
+			if(){
+				var.qtd = var.qtd + 1;	
+			}
+		}		
 	}
 }
 
